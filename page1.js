f15812036e24fa93de5b34000da80204e3986b86
D2_63018_Prasad <prasadgalande1992@gmail.com>
2022-06-20 18:17:28 +0530

    Added function isEven

--- page1.js
+++ page1.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+function isEven(number){
+    if(number%2==0){
+        console.log(`${number} is even`)
+    }else{
+        console.log(`${number} is NOT even`)
+    }
+}
+
+isEven(10)
+isEven(11)
